9bacf8a58bbfb1179aa41f0f28b5f3ee0158911f
Christabelle143 <christabelleyouta143@gmail.com>
2022-11-08 13:07:26 +0100

    refactot code for the main.rb

--- main.rb
+++ main.rb
@@ -1,51 +1,48 @@
-require_relative 'app'
+require_relative './app'
 
 def main
+  app = App.new
+  choose = nil
+
   puts 'Welcome to School Library App!'
   puts
-  app = App.new
-  app.run
-end
 
-main
+  while choose != '7'
 
-def run # rubocop:todo Metrics/CyclomaticComplexity
-  choose = select_options
-  choose = select_options while choose < 1 || choose > 7
-  case choose
-  when 1
-    books_list
-    puts 'first choose'
-  when 2
-    person_list
-  when 3
-    create_person
-  when 4
-    create_book
-  when 5
-    create_rental
-  when 6
-    rentals_list
-  when 7
-    puts 'Thank you for using the app!'
-    exit
-    # else
-    #   puts 'Choose a number between 1 to 7'
+    puts 'Please choose an option by entering a number: '
+    options = [
+      '1 - List all books',
+      '2 - List all people',
+      '3 - Create a person',
+      '4 - Create a book',
+      '5 - Create a rental',
+      '6 - List all rentals for a given person id',
+      '7 - Exit'
+    ]
+    puts options
+
+    choose = gets.chomp
+    puts "Selected: #{choose}"
+
+    case choose
+    when '1'
+      app.books_list
+      puts 'first choose'
+    when '2'
+      app.person_list
+    when '3'
+      app.create_person
+    when '4'
+      app.book_data
+    when '5'
+      app.create_rental
+    when '6'
+      app.rentals_list
+    when '7'
+      puts 'Thank you for using the app!'
+      exit
+    end
   end
 end
 
-def select_options
-  puts
-  puts 'Please choose an option by entering a number: '
-  options = [
-    '1 - List all books',
-    '2 - List all people',
-    '3 - Create a person',
-    '4 - Create a book',
-    '5 - Create a rental',
-    '6 - List all rentals for a given person id',
-    '7 - Exit'
-  ]
-  puts options
-  gets.chomp.to_i
-end
+main
